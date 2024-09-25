93cfecd52e923ddadb19271e9610c3e05707a3a2
Ketaki <Ketaki.bendre@gmail.com>
2024-09-25 11:25:27 -0700

    Error message on Save without Title and error msg on selecting same Stream, fields and agr.

--- Quantaflare.Data/EnergyStream.cs
+++ Quantaflare.Data/EnergyStream.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Quantaflare.Data
 {
@@ -12,5 +13,23 @@ namespace Quantaflare.Data
         public string? Fields { get; set; } = string.Empty;
 
         public string? agr { get; set; } = string.Empty;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is EnergyStream eStream)
+            {
+                return (Streams == eStream.Streams && Fields == eStream.Fields && agr == eStream.agr);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 31 + (Streams?.GetHashCode() ?? 0);
+            hash = hash * 31 + (Fields?.GetHashCode() ?? 0);
+            hash = hash * 31 + (agr?.GetHashCode() ?? 0);
+            return hash;
+        }
     }
 }
