48ed0fcd972ea0215a8f5dde3cbfc8df9fdfd639
Ketaki <Ketaki.bendre@gmail.com>
2024-09-26 21:52:23 -0700

    Fetaching streams from DB

--- Quantaflare.API/Controllers/StreamController.cs
+++ Quantaflare.API/Controllers/StreamController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using Quantaflare.Data;
+using System.Data;
 
 namespace Quantaflare.API.Controllers
 {
@@ -26,12 +27,50 @@ namespace Quantaflare.API.Controllers
             using (var connection = new NpgsqlConnection(_connectionString))
             {
                 //connection.Open();
-                var energyStream = connection.Query<EnergyStream>("SELECT distinct(streams) FROM energy");
+                var energyStream = connection.Query<EnergyStream>("SELECT * FROM streams");
                 return Ok(energyStream);
             }
 
 
 
+        }
+
+        [HttpGet]
+        [Route("getStreams")]
+
+        public async Task<IActionResult> getStreams(int inputId)
+        {
+            var streams = await GetStreamsId(inputId); 
+            return Ok(streams);
+        }
+
+        private async Task<IEnumerable<Streams>> GetStreamsId(int inputId)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                 db.Open();
+                var result = await db.QueryAsync<Streams>("SELECT * FROM public.get_streams(@inputId)", new { inputId });
+                return result; // Ensure you return the result here
+            }
+        }
+
+        [HttpGet]
+        [Route("getStreamAndField")]
+
+        public async Task<IActionResult> getStreamAndField(int inputId)
+        {
+            var streamAndField = await GetStreamsbyId(inputId);
+            return Ok(streamAndField);
+        }
+
+        private async Task<IEnumerable<EnergyStream>> GetStreamsbyId(int inputId)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                db.Open();
+                var result = await db.QueryAsync<EnergyStream>("SELECT * FROM public.getStreamAndField(@inputId)", new { inputId });
+                return result; // Ensure you return the result here
+            }
         }
 
         [HttpGet]
