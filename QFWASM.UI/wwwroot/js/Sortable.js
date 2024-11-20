1a59337cf3564d8edd42e72324108bf20acc010a
Ketaki Bendre <ketaki.bendre@gmail.com>
2024-11-20 14:24:01 -0800

    Swapping charts

---
+++ QFWASM.UI/wwwroot/js/Sortable.js
@@ -0,0 +1,12 @@
+﻿function initSortable(id, component) {
+    var el = document.getElementById(id);
+    if (el) {
+        new Sortable(el, {
+            animation: 200,
+            group: "sortableGroup"
+        });
+    } else {
+        console.error(`Element with ID '${id}' not found.`);
+    }
+}
+
