0289c056c57f6bb7e403ecac2310e12024226e52
Ketaki Bendre <ketaki.bendre@gmail.com>
2024-12-05 21:42:22 -0800

    maintain seprate width and some cleanup

--- Quantaflare.Data/ChartSeriesData.cs
+++ Quantaflare.Data/ChartSeriesData.cs
@@ -10,7 +10,12 @@ namespace Quantaflare.Data
     public class ChartSeriesData
     {
         public int chartId {  get; set; }
-        public string chartTitle { get; set; }
+        public string chartTitle { get; set; } = string.Empty;
+        public MudBlazor.ChartType chartType { get; set; }
+
+        public MudBlazor.ChartOptions options { get; set; } = new MudBlazor.ChartOptions();
+      
+
         public string[] xAxisLabels = Array.Empty<string>();
 
         public List<MudBlazor.ChartSeries> series = new List<MudBlazor.ChartSeries>();
