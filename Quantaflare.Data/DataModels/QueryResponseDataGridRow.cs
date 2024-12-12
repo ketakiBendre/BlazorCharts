79797a54ce34aff483b784eeebfd51a93d8c0d75
amolwankhede <amolwankhede@gmail.com>
2024-12-12 15:13:52 -0800

    adding 4 model classes

---
+++ Quantaflare.Data/DataModels/QueryResponseDataGridRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QFDomain.Entities.Data.Read
+{
+    public class QueryResponseDataGridRow
+    {
+        public long TimeStamp { get; set; }
+        public Dictionary<string, object> KeyValues { get; set; } = new Dictionary<string, object>();
+    }
+
